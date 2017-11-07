909abb2b6679d6d779e52e31308c8039684320eb
shumcode <cshumylo@gmail.com>
2017-11-06 22:32:17 -0500

    too tired to do anymore

---
+++ lab part 1/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $(".available").mouseenter(function() {
+    $(this).fadeTo(150, 0.5);
+  });
+  $(".available").mouseleave(function() {
+    $(this).fadeTo(0, 1);
+  });
+  $(".available").click(function(){
+    var roomNumber = $(this).text();
+    $("form").css("display", "flex");
+    $(this).attr("class", "reserved");
+    $(this).attr("class2", "true");
+    $(".tablenumber").text("Table Number: " + roomNumber);
+  });
+  $("i").click(function() {
+    $("form").css("display", "none");
+    $("div").filter("[class2=true]").attr("class", "available");
+  });
+  $("#save").click(function() {
+    $("form").css("display", "none");
+    $("div").filter("[class2=true]").attr("class2", "false");
+    $("div").filter("[class2=false]").css("cursor", "not-allowed");
+  });
+});
